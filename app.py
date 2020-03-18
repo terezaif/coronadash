b815ec90ec7dca1370b0b61f49b7b73a797cde26
Tereza Iofciu <t.iofciu@mytaxi.com>
2020-03-18 12:40:28 +0100

    set day 0 to 100 cases

--- app.py
+++ app.py
@@ -33,8 +33,9 @@ data = pd.read_json('https://api.covid19api.com/all')
 data["Date"] = pd.to_datetime(data["Date"], errors='coerce').dt.date
 data["CountryProvince"] = data["Country"]+data["Province"]
 #getting start date of contagion
+min_cases = 100
 temp_first = data[(data['Status']=='confirmed') & (data['Cases']>0)].groupby(['CountryProvince'])['Date'].min().reset_index().rename(columns={"Date": "FirstDate"})
-temp_max = data[(data['Status']=='confirmed') & (data['Cases']>=0)].groupby(['CountryProvince'])['Cases'].max().reset_index().rename(columns={"Cases": "MaxCases"})
+temp_max = data[(data['Status']=='confirmed') & (data['Cases']>=min_cases)].groupby(['CountryProvince'])['Cases'].max().reset_index().rename(columns={"Cases": "MaxCases"})
 #adding start date of contagion to data
 data = data.set_index('CountryProvince')
 data = data.join(temp_first.set_index('CountryProvince'))
@@ -42,7 +43,8 @@ data = data.join(temp_max.set_index('CountryProvince'))
 data["DaysSinceConfirmed"] = np.where(data['Date']>=data['FirstDate'], (data['Date']-data['FirstDate']).dt.days, 0)
 
 status = "confirmed"
-temp = data[(data["MaxCases"]>=500) & (data["Status"] == status)].copy()
+min_total_cases = 500
+temp = data[(data["MaxCases"]>=min_total_cases) & (data["Status"] == status)].copy()
 
 provinces = temp.index.unique()
 plotdata = []
@@ -59,8 +61,8 @@ for province in provinces:
     plotdata.append(trace)
 
 layout_conf = go.Layout(
-    title = '(Logarithmic) Daily confirmed cases per Country/Province where more than 1000 cases have been registered',
-    xaxis = dict(title = 'Day since first confirmed local case'), # x-axis label
+    title = '(Logarithmic) Daily confirmed cases per Country/Province where more than {} cases have been registered'.format(min_total_cases),
+    xaxis = dict(title = 'Day since first {} confirmed local case'.format(min_cases)), # x-axis label
     yaxis = dict(title = 'Confirmed cases'), # y-axis label
     yaxis_type='log',
     hovermode='closest'
@@ -71,7 +73,7 @@ app.layout = html.Div(
     children=[
     html.H1(children='Country/Province comparison '),
     html.Div(children='Virus spread by country and day'),
-    html.Div(children='Showing only countries/regions that have reached 500 confirmed cases'),
+    html.Div(children='Showing only countries/regions that have reached {} confirmed cases'.format(min_total_cases)),
     html.A('Data source', href='https://covid19api.com'),
     dcc.Graph(
         id='log_confirmed',
